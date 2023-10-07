8bd9e37cf139b9869916535137355708c3e8804b
Hybee <hybee234@outlook.com>
2023-10-07 22:45:27 +1100

    Changes to API URL, JS dynamic results creation, commenting aand more

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-// RECIPE API
-let recipeArray = []; // Using "let" to declare recipeArray as it will be modified later
 
-
-// SEARCH BUTTON / SEARCH BOX VARIABLES
+// Elements
 const searchInput = document.getElementById('search');
 const searchBtn = document.querySelector('.search-btn'); 
+const featuredTableEl = document.querySelector("#recipe-table-featured");    //Feature table element
+const resultsTableEl = document.querySelector("#recipe-table-results");     //Recipe results table element
 
+const appKey = "bd89c9d8361609dbed2adb82d1106d40";      //Edamam App Key (Mahmoud)
+const appID = "45b75717"                                //Edamam Recipe App ID
+
+//Arrays
+let recipeArray = []; // Using "let" to declare recipeArray as it will be modified later
 
 //-------------------------------------//
 //- FUNCTION - FETCH RECIPES FROM API -//
@@ -13,26 +17,29 @@ const searchBtn = document.querySelector('.search-btn');
 
 function fetchRecipes(searchTerm) {
     console.log("\n\n\n> fetchRecipes() Called");  
-    const recipeUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=45b75717&app_key=bd89c9d8361609dbed2adb82d1106d40&imageSize=THUMBNAIL&field=label&field=url&field=calories&q=${searchTerm}`;
+    const recipeUrl = "https://api.edamam.com/api/recipes/v2?type=public&app_id=" + appID + "&app_key=" + appKey + "&field=uri&field=label&field=image&field=images&field=source&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=glycemicIndex&field=totalCO2Emissions&field=co2EmissionsClass&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients&field=totalDaily&field=digest&field=tags&field=externalId&q="+ searchTerm;   
+    
     console.log("  Fetching recipes from edamam...")
     fetch(recipeUrl)                                                                            // Fetch data from edamam using URL above
         .then(function (response) {
             console.log("  ... recipes received from Edamam.")
             if (response.ok) {                                                                  // Check if response OK             
                 response.json().then(function (data) {                                          // Hold API response in 'data'
-                    console.log (data.hits.length)
+                    console.log("  Checking data received:")
+                    console.log("    data.hits.length = " + data.hits.length)
                     if (data.hits.length === 0) {                                               // Check if 'data.hits' has values. Alert if zero (data.hits is where recipes are returned in the JSON object
+                        console.log("    No recipes found - bad")
                         alert('No recipes were found - please review your search term(s) and try again');                        
                         return;
-                    } else {
-                        console.log (data.length)
+                    } else {                        
+                        console.log("    Recipes found - good")
                         console.log("  Sending recipes to local storage ('key = recipes')");                        
-                        localStorage.setItem('recipes', JSON.stringify(recipeArray));  // STORING FETCHED DATA IN LOCAL STORAGE
-                        recipeArray = data; // Store the fetched data in recipeArray 
+                        localStorage.setItem('recipes', JSON.stringify(recipeArray));           // STORING FETCHED DATA IN LOCAL STORAGE
+                        recipeArray = data;                                                     // Store the fetched data in recipeArray 
                         console.log("  Storing API data in global variable 'recipeArray'");
                         console.log("    recipeArray:\n    ------------");                        
-                        console.log(recipeArray);
-                        console.log (recipeArray.hits.length);
+                        console.log(recipeArray);       
+                        displayRecipes();                 
                     }
                 });
             } else {
@@ -48,36 +55,89 @@ function fetchRecipes(searchTerm) {
 //- FUNCTION - DISPLAY RECIPES -//
 //------------------------------//
 
+function displayRecipes() {
+    console.log("\n\n\n> displayRecipes() Called");  
+    console.log("  Hiding 'feature-recipe-table' ");  
+    featuredTableEl.style.display = "none";                         // hide feature-recipe-table
+    console.log("  Show 'recipe-results-table' ");  
+    resultsTableEl.style.display = "inline";
+    console.log("  Clearing resultsTableEl to make way for new results")         
+    resultsTableEl.innerHTML = "" ;                                                 // Remove all child elements under resultsTableEl (cleras recipe results) - otherwise they'll keep appending
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//--------------------------------------------------------------//
-//- LISTENER - PAGE LOAD - RETRIEVE RECIPES FROM LOCAL STORAGE -//
-//--------------------------------------------------------------//
-
-window.addEventListener('load', function () {
-    console.log("\n\n\n! Page load triggered");      
-    const savedRecipes = localStorage.getItem('recipes');
-    if (savedRecipes) {
-        recipeArray = JSON.parse(savedRecipes);
+    for (let i = 0; i < recipeArray.hits.length; i++) {
         
-        console.log("  Recipes retrieved from local storage ('key = recipes'):");
-        console.log("    recipeArray:\n    ------------")
-        console.log(recipeArray);
+        if (i === (recipeArray.hits.length-1)) {
+            console.log("  All recipes rendered")                                   // Console.log a message to indicate all recipes rendered
+        };
+
+        var recipeContainerEl = document.createElement('li');                      // Create recipe container (li) - this will be appended to main 'ul" container (resutlsTableEl)
+        recipeContainerEl.classList = "recipe-container" ;                         // Add class to element
+        resultsTableEl.appendChild(recipeContainerEl);                             // Append recipeContinerEl to resultsTableEl
+
+            var linkContainerEl = document.createElement('div');                    // Create link container - one of 2 child element to recipe container
+            linkContainerEl.classlist = "link-container";                          // Add class to element
+            recipeContainerEl.appendChild(linkContainerEl);                         // Append link container to recipe container
+
+                var linkEl = document.createElement ('a');                       // Create link element                
+                linkEl.classlist = "link-image"                                  // Assign class
+                linkEl.href = recipeArray.hits[i].recipe.url;                    // Define link
+                linkContainerEl.appendChild(linkEl);                            // Append link element to link contaainer
+                                
+                    var imageEl = document.createElement ('img')                              // Create image element
+                    imageEl.src = recipeArray.hits[i].recipe.images.SMALL.url;                // Define image source
+                    imageEl.alt = "image and link for " + recipeArray.hits[i].recipe.label;   // Alt text for accessibility
+                    linkEl.appendChild(imageEl);                                              // Append link to image
+
+            var recipeDetailContainerEl = document.createElement('div')             // Create Recipe detail container - one of 2 child elements to recipe container 
+            recipeDetailContainerEl.classlist = "recipe-detail-container";          // Add class to element
+            recipeContainerEl.appendChild(recipeDetailContainerEl);                 // Append Recipe Detaail Container to Recipe Container
+
+                var recipeLabelEl = document.createElement('h2');            // Recipe label (heading) - child element to recipeDetailContainer
+                recipeLabelEl.classlist = "recipe-title";                    // Add class 
+                recipeLabelEl.textContent = recipeArray.hits[i].recipe.label;// Add recipe title
+                recipeDetailContainerEl.appendChild(recipeLabelEl);          // Append Recipe Label to recipeDetail Container
+
+                var recipeDetailEl = document.createElement('p');           // Recipe detils - child element to recipeDetailContainer
+                recipeDetailEl.classlist = "receipt-detail";                // Add claass
+                recipeDetailEl.textContent = "Calories" + recipeArray.hits[i].recipe.calories + ", Cuisine Type: " + recipeArray.hits[i].recipe.cuisineType + ", Dish Type: " + recipeArray.hits[i].recipe.dishType + ", Meal Type: " + recipeArray.hits[i].recipe.mealType[0] + ", Time: " + recipeArray.hits[0].recipe.totalTime + ", Makes: " + recipeArray.hits[0].recipe.yield;  // Add Text
+                recipeDetailContainerEl.appendChild(recipeDetailEl);        // Append Recipe Details to recipeDetaail Container
+
+                var recipeLinkEl = document.createElement('a');             // Recipe Link - child element to recipeDetailContainer
+                recipeLinkEl.classlist = "link-recipe";                     // Add class 
+                recipeLinkEl.href = recipeArray.hits[i].recipe.url;         // Add Recipe Link
+                recipeLinkEl.textContent = recipeArray.hits[i].recipe.url;  // Add link text
+                recipeLinkEl.style.color = "blue";                          // Style link text
+                recipeDetailContainerEl.appendChild(recipeLinkEl);          // Append Recipe Link to recipeDetailContainer
+ 
+                var recipeSourceEl = document.createElement('p');           // Recipe Source - child element to recipeDetailContainer
+                recipeSourceEl.classlist = "recipe-source";                 // Add class
+                recipeSourceEl.textContent = recipeArray.hits[i].recipe.source; // Add source text      
+                recipeDetailContainerEl.appendChild(recipeSourceEl);            //Append Recipe Source to recipeDetailContainer
+
     }
-});
+  
+
+};
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //----------------------------------//
 //- LISTENER - CLICK SEARCH BUTTON -//
@@ -114,3 +174,23 @@ searchInput.addEventListener('keydown', function (event) {                   // 
         }
     }
 });
+
+
+//--------------------------------------------------------------//
+//- LISTENER - PAGE LOAD - RETRIEVE RECIPES FROM LOCAL STORAGE -//
+//--------------------------------------------------------------//
+
+window.addEventListener('load', function () {
+    console.log("\n\n\n! Page load triggered");      
+    const savedRecipes = localStorage.getItem('recipes');
+    if (savedRecipes) {
+        recipeArray = JSON.parse(savedRecipes);
+        
+        console.log("  Recipes retrieved from local storage ('key = recipes'):");
+        console.log("    recipeArray:\n    ------------")
+        console.log(recipeArray);
+    }
+    console.log("  Hiding 'recipe-results-table' ");  
+    resultsTableEl.style.display = "none";     
+});
+ 
