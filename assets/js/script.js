c986fff903a8afeb63abfe0ecd4890ba3d860344
Hybee <hybee234@outlook.com>
2023-10-07 18:09:21 +1100

    Corrected reset.css filepath in HTML, updated JS to include more detailed console logging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,33 @@
+// RECIPE API
+let recipeArray = []; // Using "let" to declare recipeArray as it will be modified later
+
+
 // SEARCH BUTTON / SEARCH BOX VARIABLES
 const searchInput = document.getElementById('search');
 const searchBtn = document.querySelector('.search-btn'); 
 
 // EVENT LISTENERS FOR SEARCH BUTTON
 searchBtn.addEventListener('click', function (event) {
+    console.log("\n\n\n! Search button click triggered");      
     event.preventDefault(); // Prevent form submission
     const searchTerm = searchInput.value;
     if (searchTerm) {
+        console.log("  calling fetchRecipes('" + searchTerm + "')"); 
         fetchRecipes(searchTerm); // Call the fetchRecipes function with the search term
     } else {
         alert('Please enter a search term.');
-    }
-    console.log(`Searching for "${searchTerm}"...`);
+    }    
 });
 
 // EVENT LISTENER FOR INPUT (USING ENTER KEYDOWN)
 searchInput.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
+    console.log("\n\n\n! Keydown event triggered");      
+    if (event.key === 'Enter') {     // If the keydown event was triggered by the "Enter" key
+        console.log("  'Enter' detected");  
         event.preventDefault();
         const searchTerm = searchInput.value;
         if (searchTerm) {
+            console.log("  calling fetchRecipes('" + searchTerm + "')");  
             fetchRecipes(searchTerm); // Call the fetchRecipes function with the search term
         } else {
             alert('Please enter a search term.');
@@ -27,27 +35,25 @@ searchInput.addEventListener('keydown', function (event) {
     }
 });
 
-// RECIPE API
-let recipeArray = []; // Using "let" to declare recipeArray as it will be modified later
-
 // FUNCTION TO FETCH RECIPES FROM API
 function fetchRecipes(searchTerm) {
+    console.log("\n\n\n> fetchRecipes() Called");  
     const recipeUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=45b75717&app_key=bd89c9d8361609dbed2adb82d1106d40&imageSize=THUMBNAIL&field=label&field=url&field=calories&q=${searchTerm}`;
-
+    console.log("  Fetching recipes from edamam...")
     fetch(recipeUrl)
         .then(function (response) {
-            if (response.ok) {
+            console.log("  ... recipes received from Edamam.")
+            if (response.ok) {                
                 response.json().then(function (data) {
                     if (data.length === 0) {
                         alert('No recipe found - please try again');
                         return;
                     } else {
-                        recipeArray = data; // Store the fetched data in recipeArray
-                        
-                        // STORING FETCHED DATA IN LOCAL STORAGE
-                        localStorage.setItem('recipes', JSON.stringify(recipeArray));
-
-                        console.log("Recipe API data stored in global variable 'recipeArray'");
+                        console.log("  Sending recipes to local storage ('key = recipes')");                        
+                        localStorage.setItem('recipes', JSON.stringify(recipeArray));  // STORING FETCHED DATA IN LOCAL STORAGE
+                        recipeArray = data; // Store the fetched data in recipeArray 
+                        console.log("  Storing API data in global variable 'recipeArray'");
+                        console.log("    recipeArray:\n    ------------");                        
                         console.log(recipeArray);
                     }
                 });
@@ -62,11 +68,13 @@ function fetchRecipes(searchTerm) {
 
 // LOCAL DATA FROM LOCAL STORAGE WHEN PAGE LOADS
 window.addEventListener('load', function () {
+    console.log("\n\n\n! Page load triggered");      
     const savedRecipes = localStorage.getItem('recipes');
     if (savedRecipes) {
         recipeArray = JSON.parse(savedRecipes);
         
-        console.log('Recipe data loaded from local storage');
+        console.log("  Recipes retrieved from local storage ('key = recipes'):");
+        console.log("    recipeArray:\n    ------------")
         console.log(recipeArray);
     }
 });
