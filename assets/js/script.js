e210e91440c3336819771b6ef47e86753c7382d7
Hybee <hybee234@outlook.com>
2023-10-07 18:28:26 +1100

    Tweaks to error checking API fetch results - more console log tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,55 +6,33 @@ let recipeArray = []; // Using "let" to declare recipeArray as it will be modifi
 const searchInput = document.getElementById('search');
 const searchBtn = document.querySelector('.search-btn'); 
 
-// EVENT LISTENERS FOR SEARCH BUTTON
-searchBtn.addEventListener('click', function (event) {
-    console.log("\n\n\n! Search button click triggered");      
-    event.preventDefault(); // Prevent form submission
-    const searchTerm = searchInput.value;
-    if (searchTerm) {
-        console.log("  calling fetchRecipes('" + searchTerm + "')"); 
-        fetchRecipes(searchTerm); // Call the fetchRecipes function with the search term
-    } else {
-        alert('Please enter a search term.');
-    }    
-});
 
-// EVENT LISTENER FOR INPUT (USING ENTER KEYDOWN)
-searchInput.addEventListener('keydown', function (event) {
-    console.log("\n\n\n! Keydown event triggered");      
-    if (event.key === 'Enter') {     // If the keydown event was triggered by the "Enter" key
-        console.log("  'Enter' detected");  
-        event.preventDefault();
-        const searchTerm = searchInput.value;
-        if (searchTerm) {
-            console.log("  calling fetchRecipes('" + searchTerm + "')");  
-            fetchRecipes(searchTerm); // Call the fetchRecipes function with the search term
-        } else {
-            alert('Please enter a search term.');
-        }
-    }
-});
+//-------------------------------------//
+//- FUNCTION - FETCH RECIPES FROM API -//
+//-------------------------------------//
 
-// FUNCTION TO FETCH RECIPES FROM API
 function fetchRecipes(searchTerm) {
     console.log("\n\n\n> fetchRecipes() Called");  
     const recipeUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=45b75717&app_key=bd89c9d8361609dbed2adb82d1106d40&imageSize=THUMBNAIL&field=label&field=url&field=calories&q=${searchTerm}`;
     console.log("  Fetching recipes from edamam...")
-    fetch(recipeUrl)
+    fetch(recipeUrl)                                                                            // Fetch data from edamam using URL above
         .then(function (response) {
             console.log("  ... recipes received from Edamam.")
-            if (response.ok) {                
-                response.json().then(function (data) {
-                    if (data.length === 0) {
-                        alert('No recipe found - please try again');
+            if (response.ok) {                                                                  // Check if response OK             
+                response.json().then(function (data) {                                          // Hold API response in 'data'
+                    console.log (data.hits.length)
+                    if (data.hits.length === 0) {                                               // Check if 'data.hits' has values. Alert if zero (data.hits is where recipes are returned in the JSON object
+                        alert('No recipes were found - please review your search term(s) and try again');                        
                         return;
                     } else {
+                        console.log (data.length)
                         console.log("  Sending recipes to local storage ('key = recipes')");                        
                         localStorage.setItem('recipes', JSON.stringify(recipeArray));  // STORING FETCHED DATA IN LOCAL STORAGE
                         recipeArray = data; // Store the fetched data in recipeArray 
                         console.log("  Storing API data in global variable 'recipeArray'");
                         console.log("    recipeArray:\n    ------------");                        
                         console.log(recipeArray);
+                        console.log (recipeArray.hits.length);
                     }
                 });
             } else {
@@ -66,7 +44,29 @@ function fetchRecipes(searchTerm) {
         });
 }
 
-// LOCAL DATA FROM LOCAL STORAGE WHEN PAGE LOADS
+//------------------------------//
+//- FUNCTION - DISPLAY RECIPES -//
+//------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------//
+//- LISTENER - PAGE LOAD - RETRIEVE RECIPES FROM LOCAL STORAGE -//
+//--------------------------------------------------------------//
+
 window.addEventListener('load', function () {
     console.log("\n\n\n! Page load triggered");      
     const savedRecipes = localStorage.getItem('recipes');
@@ -76,5 +76,41 @@ window.addEventListener('load', function () {
         console.log("  Recipes retrieved from local storage ('key = recipes'):");
         console.log("    recipeArray:\n    ------------")
         console.log(recipeArray);
+    }
+});
+
+//----------------------------------//
+//- LISTENER - CLICK SEARCH BUTTON -//
+//----------------------------------//
+
+searchBtn.addEventListener('click', function (event) {                       // Listens for event where user clicks on button (with magnifying glass)
+    console.log("\n\n\n! Search button click triggered");      
+    event.preventDefault();                                                  // Prevent page refresh
+    const searchTerm = searchInput.value;                                    // 'searchTerm' to equal value in search field on page
+    if (searchTerm) {
+        console.log("  calling fetchRecipes('" + searchTerm + "')"); 
+        fetchRecipes(searchTerm);                                            // Call the fetchRecipes function passing through the value on searchTerm onto fetchRecipes()
+    } else {
+        alert('Please enter a search term.');                                // If searchTerm is falsy then present alert to user
+    }    
+});
+
+
+//----------------------------//
+//- LISTENER - ENTER KEYDOWN -//
+//----------------------------//
+
+searchInput.addEventListener('keydown', function (event) {                   // Listens for event where a key is pressed down (any)
+    console.log("\n\n\n! Keydown event triggered");      
+    if (event.key === 'Enter') {                                             // If the keydown event was triggered by the "Enter" key
+        console.log("  'Enter' detected");  
+        event.preventDefault();                                              // Prevent page refresh
+        const searchTerm = searchInput.value;                                // 'searchTerm' to equal value in search field on page
+        if (searchTerm) {
+            console.log("  calling fetchRecipes('" + searchTerm + "')");  
+            fetchRecipes(searchTerm);                                        // Call the fetchRecipes function passing through the value on searchTerm onto fetchRecipes()
+        } else {
+            alert('Please enter a search term.');                            // If searchTerm is falsy then present alert to user
+        }
     }
 });
