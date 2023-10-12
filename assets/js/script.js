a7b602a1d9be383bf51a36845e0740059f4ac08d
mingosis <tom.piccirillo15@gmail.com>
2023-10-12 16:11:02 +1000

    fixing dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,7 +105,7 @@ function displayRecipes() {
         };
 
         var recipeContainerEl = document.createElement('li');                                                                                             // Create recipe container (li) - this will be appended to main 'ul" container (resutlsTableEl)
-        recipeContainerEl.classList.add ("recipe-container", "border-8", "rounded-3xl", "flex", "flex-wrap", "m-5", "border-emerald-400", "bg-white");                // Add class (tailwind style)
+        recipeContainerEl.classList.add ("recipe-container", "border-8", "rounded-3xl", "flex", "flex-wrap", "m-5", "border-emerald-400", "bg-white", "dark:bg-slate-900");                // Add class (tailwind style)
         resultsTableEl.appendChild(recipeContainerEl);                                                                                                    // Append recipeContinerEl to resultsTableEl
 
                 var linkContainerEl = document.createElement('div');                                                                                      // Create link container - one of 2 child element to recipe container
